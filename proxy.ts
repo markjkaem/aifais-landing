65136a9aa0d170eeb2a17365a3fcf3c89c24bf71
markjkaem <markteek@gmail.com>
2025-12-26 22:17:17 +0100

    removed middleware name convention

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware({
+export const proxy = createMiddleware({
   locales: ['nl', 'en'],
   defaultLocale: 'nl',
   localePrefix: 'as-needed',
